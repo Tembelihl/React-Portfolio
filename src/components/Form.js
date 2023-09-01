27e7a2668b773c4e7add8e3f8b2ea467d4816736
lihle <lihlebiyana1@gmail.com>
2023-09-01 12:15:20 +0200

    I try adding EmailJs in my form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,34 +1,38 @@
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import "./FormStyles.css";
-import React from 'react'
-import {useState} from 'react'
+import React from "react";
 
 const Form = () => {
-    const [data, setData] = useState({name:"", email:"", message:""});
-    const handleChange = (e) =>{
-        const name = e.target.name;
-        const value = e.target.value;
-        setData({...data, [name]: value})
-    }
+  const form = useRef();
 
-    const handleSubmit = (e) =>{
-        e.prevenDefault()
-        alert(data)
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-    }
+    emailjs.sendForm('service_a4mgkfm', 'template_96dm2fq', form.current, '1q-hx506alRfH1SRC')
+      .then((result) => {
+          console.log(result.text);
+          console.log("Message Sent Successfully")
+      }, (error) => {
+          console.log(error.text);
+      }
+      );
+  };
+    
   return <div className="form">
-    <h1>Contact <span>Here</span></h1>
-    <form method='post' onSubmit={handleSubmit}>
-    <label>Full Name</label>
-    <input type="text" name="name" id="" onChange={handleChange} value={data.name} placeholder="Enter Name"/>
-    <label>Email</label>
-    <input type="email" name="email" id="" onChange={handleChange} value={data.email} placeholder="example@gmail.com"/>
-    <label>Phone</label>
-    <input type="phone" name="phone" id="" onChange={handleChange} value={data.phone} placeholder="+27"/>
-    <label>Message</label>
-    <textarea name="message" id="" cols="30" onChange={handleChange} value={data.message} row="10" placeholder="Type message Here..."/>
-    <button type='submit'>Send Message</button>
-  </form>
-</div>
-};
+  <h1>Contact <span>Here</span></h1>
+      <form ref={form} onSubmit={sendEmail}>
+      <label> Full Name</label>
+      <input type="text" name="Enter Name" required/>
+      <label>Email</label>
+      <input type="email" name="example@gmail.com" required/>
+      <label>Phone</label>
+      <input type="phone" name="+27" required/>
+      <label>Message</label>
+      <textarea name=" Type Message Here..." required/>
+      <button type="submit>"> Send Message</button>
+    </form>
+    </div> 
 
-export default Form
+    };
+export default Form;
